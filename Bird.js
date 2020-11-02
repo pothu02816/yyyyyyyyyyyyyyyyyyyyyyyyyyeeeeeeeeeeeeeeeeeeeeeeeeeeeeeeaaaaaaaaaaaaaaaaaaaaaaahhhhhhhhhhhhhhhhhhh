00990693fde6bed1403f88a2af5bd85a26e20f7b
pothu02816 <pothu02816@loswego.k12.or.us>
2020-11-01 18:48:14 -0800

    trajectory

--- Bird.js
+++ Bird.js
@@ -2,6 +2,8 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.someimage=loadImage("sprites/smoke.png")
+    this.trajecotry=[]
   }
 
   display() {
@@ -9,5 +11,12 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    var position=[this.body.position.x,this.body.position.y]
+    this.trajecotry.push(position);
+    for (var i =0;i<this.trajecotry.length;i++){
+      
+
+      image(this.someimage,this.trajecotry[i],[0],[this.trajecotry],[i1])
+    }
   }
 }
